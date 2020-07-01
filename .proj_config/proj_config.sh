aa551bb58fe70459880dc80b7b1f1db30f847823
rjf12138 <3161173206@qq.com>
2020-07-01 22:58:45 +0800

    first commit

---
+++ .proj_config/proj_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# basic config
+export PROJ_PROJECT_PATH=/home/ruanjian/workspace/project/huffman_compress
+export PROJ_VSCODE_CONFIG_PATH=$PROJ_PROJECT_PATH/.vscode
+export PROJ_UUID=9649817b-6ddc-429b-a1aa-027cada1e64d
+export PROJ_PROJECT_NAME=huffman_compress
+export PROJ_EXEC_NAME=${PROJ_PROJECT_NAME}_exe
+export PROJ_EXEC_ARGS=""
+export PROJ_LIB_NAME=${PROJ_PROJECT_NAME}
+export PROJ_CMAKE_FILE=CMakeLists.txt
+export PROJ_LIB_LINK_LIST=""
+export PROJ_LIB_OUTPUT_DIR=$PROJ_PROJECT_PATH/output
+export PROJ_BIN_OUTPUT_DIR=$PROJ_PROJECT_PATH/output
+
+#compile and debug config
+export COMPILE_SCRIPT_NAME=project
+export COMPILE_SCRIPT_PATH=project
+export COMPILE_SCRIPT_ARGS="-r"
+
